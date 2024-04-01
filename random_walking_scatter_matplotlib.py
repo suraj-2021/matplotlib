58f7ff41537cf0f406669ea39259fe6c6e6dcf20
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-04-01 09:41:04 +0530

    Create random_walking_scatter_matplotlib.py

---
+++ random_walking_scatter_matplotlib.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from random import choice
+
+class RandomWalk:
+    def __init__(self, num_points= 5000):
+        self.num_points = num_points
+        self.x_values=[0]
+        self.y_values=[0]
+    def fill_walk(self):
+        while len(self.x_values)<= self.num_points:
+              x_direction = choice([0,-1])
+              x_distance = choice([0,1,2,3,4])
+              x_step = x_direction * x_distance
+              
+              y_direction = choice([0,-1])
+              y_distance = choice ([0,1,2,3,4,5])
+              y_step = y_direction * y_distance
+              
+              if x_step == 0 and y_step == 0:
+                  continue
+              x =  self.x_values[-1]+x_step
+              y =  self.y_values[-1]+ y_step
+              
+              self.x_values.append(x)
+              self.y_values.append(y)
+lk = RandomWalk()
+lk.fill_walk()
+fig,ax = plt.subplots()
+point_numbers = [i**2 for i in range(0,5001)]
+plt.style.use("fivethirtyeight")
+
+ax.scatter(lk.x_values,lk.y_values,c=point_numbers, cmap = plt.cm.Blues,edgecolors = 'none', s=10)
+plt.show()
+              
+              
