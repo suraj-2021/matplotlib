722c14c6878b5a0e4fe2117ee40273f186d6660d
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-04-17 21:22:43 +0530

    Create sample_matplotlib.py

---
+++ sample_matplotlib.py
@@ -0,0 +1,21 @@
+# Importing the required module
+import matplotlib.pyplot as plt
+
+# X-axis values
+x = [1, 2, 3]
+
+# Corresponding Y-axis values
+y = [2, 4, 1]
+
+# Plotting the data
+plt.plot(x, y)
+
+# Naming the axes
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+
+# Giving a title to the graph
+plt.title('My first graph!')
+
+# Display the plot
+plt.show()
